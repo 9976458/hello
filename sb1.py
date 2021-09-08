0217ae5fb976709c9828c173ed4f80b8b5d6bcb1
9976458 <90094580+9976458@users.noreply.github.com>
2021-09-08 21:01:44 +0800

    Add files via upload

---
+++ sb1.py
@@ -0,0 +1,11 @@
+# sb1.py
+
+import os
+import json
+
+def get(url,args,self):
+    filename = self.args['path']
+    if os in ('.xml','.kt'):
+        return self._dat101(__name__,317)
+        for i in range(88):
+            print('1')
