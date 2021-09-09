021a1f3a40c4a31a78134db28ded9cfc4ac55446
9976458 <90094580+9976458@users.noreply.github.com>
2021-09-09 20:31:43 +0800

    Add files via upload

---
+++ ruby3.rb
@@ -0,0 +1,24 @@
+public class ak
+    loop do
+        task.params(.admin)
+    end
+    class show
+        def module hi
+            loop do
+                { |variable|put|  
+            end
+        end
+    end
+end
+private class m4
+    namespace : do
+        desc "hello"
+        task : :environment do
+            unless test
+                for 2 in enumerable do
+                    put "20"
+                end
+            end
+        end
+    end
+end
