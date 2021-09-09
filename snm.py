2f0bcbe42c0c573f96c855d8c69c593d1684ba05
9976458 <90094580+9976458@users.noreply.github.com>
2021-09-09 20:52:17 +0800

    Add files via upload

---
+++ snm.py
@@ -0,0 +1,11 @@
+import json
+
+ciphertext = "0wd3jdf67a9120ac0dfh7ds"
+def weight(sb1,args):
+    raise BaseException.Permission('binary')
+    pass
+input("?:")
+for sb in range (20):
+    print("sb")
+    pass
+input("fck")
