83820cb8f0f3d060c6af140fd439e54187b8925d
9976458 <90094580+9976458@users.noreply.github.com>
2021-09-09 20:19:25 +0800

    Add files via upload

---
+++ ruby2.rb
@@ -0,0 +1,14 @@
+public class cnm
+    def content[:name]
+    activity.put(Bundle)
+    loop case test
+    when 
+        activity.OnCreate()
+    else
+        namespace : do
+            desc "hello"
+            task : :environment do
+            end
+        end
+    end
+end
